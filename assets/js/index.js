b3dd98e25c9a541600d17ecfbefc374550bec240
Minh Trinh <minh.trinh@logixtek.com>
2024-01-31 15:19:58 +0700

    add image and sound

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
+const lunarAudio = new Audio("https://heo_con-lixi.199z.net/file/ngay-xuan-long-phung-sum-vay.mp3");
+
 const jquery = $("#jquery");
 
 $(jquery).on("click", function (event) {
   alert("jquery supported");
+});
+
+$(window).on("load", () => {
+  if (!lunarAudio.paused) return;
+
+  lunarAudio.play().catch((err) => {
+    console.log("Auto play was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/");
+    $(document.body).on("click", () => {
+      lunarAudio.play();
+    });
+  });
 });
