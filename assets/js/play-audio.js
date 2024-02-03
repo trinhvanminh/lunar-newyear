edb59718e61fcbbf3b76e2d122c3d6485d059183
vu.nguyen <ntruongvux@gmail.com>
2024-02-04 02:24:53 +0700

    Add music to static file

--- assets/js/play-audio.js
+++ assets/js/play-audio.js
@@ -1,23 +1,14 @@
-const lunarAudio = new Audio(
-  "https://heo_con-lixi.199z.net/file/ngay-xuan-long-phung-sum-vay.mp3"
-);
-
-const jquery = $("#jquery");
-
-$(jquery).on("click", function (event) {
-  alert("jquery supported");
-});
+const lunarAudio = new Audio("/assets/music/ngay-xuan-long-phung-sum-vay.mp3");
 
 $(window).on("load", () => {
   if (!lunarAudio.paused) return;
 
-  // TODO
-  // lunarAudio.play().catch((err) => {
-  //   console.log(
-  //     "Auto play was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
-  //   );
-  //   $(document.body).on("click", () => {
-  //     lunarAudio.play();
-  //   });
-  // });
+  lunarAudio.play().catch(() => {
+    console.log(
+      "Auto play was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
+    );
+    $(document.body).on("click", () => {
+      lunarAudio.play();
+    });
+  });
 });
