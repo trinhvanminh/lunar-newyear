88a84d8d26ec625793f38604c478386d3e675084
vu.nguyen <ntruongvux@gmail.com>
2024-02-04 11:13:22 +0700

    Add play/pause audio button

--- assets/js/play-audio.js
+++ assets/js/play-audio.js
@@ -1,14 +1,52 @@
 const lunarAudio = new Audio("/assets/music/ngay-xuan-long-phung-sum-vay.mp3");
-
 $(window).on("load", () => {
-  if (!lunarAudio.paused) return;
-
-  lunarAudio.play().catch(() => {
-    console.log(
-      "Auto play was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
-    );
-    $(document.body).on("click", () => {
-      lunarAudio.play();
+  function play() {
+    lunarAudio.play().catch(() => {
+      console.error(
+        "Audio was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
+      );
     });
+
+    $(BUTTON_PAUSE_AUDIO_SELECTOR).removeClass("hidden");
+    $(BUTTON_PAUSE_AUDIO_SELECTOR).addClass("active");
+  }
+
+  function pause() {
+    lunarAudio.pause().catch(() => {
+      console.error(
+        "Audio was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
+      );
+    });
+
+    $(BUTTON_PAUSE_AUDIO_SELECTOR).removeClass("active");
+  }
+
+  $(BUTTON_OK_SELECTOR).click(function () {
+    if ($(BUTTON_PAUSE_AUDIO_SELECTOR).hasClass("hidden")) {
+      play();
+    }
   });
+
+  $(BUTTON_CANCEL_SELECTOR).click(function () {
+    if ($(BUTTON_PAUSE_AUDIO_SELECTOR).hasClass("hidden")) {
+      play();
+    }
+  });
+
+  $(BUTTON_PAUSE_AUDIO_SELECTOR).click(function () {
+    if (lunarAudio.paused) {
+      play();
+    } else {
+      pause();
+    }
+  });
+
+  // lunarAudio.play().catch(() => {
+  //   console.log(
+  //     "Auto play was disabled by Chrome policy, https://developer.chrome.com/blog/autoplay/"
+  //   );
+  //   $(document.body).on("click", () => {
+  //     lunarAudio.play();
+  //   });
+  // });
 });
