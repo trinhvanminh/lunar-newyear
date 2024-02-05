540990be2f051558958a3952542787f85f01cb2e
Minh Trinh <minh.trinh@logixtek.com>
2024-02-05 16:16:53 +0700

    add music to chucmung page

--- assets/js/chuc-mung.js
+++ assets/js/chuc-mung.js
@@ -1,24 +1,46 @@
+const toothlessDragonAudio = new Audio("../assets/music/toothless-dance.mp3");
+
 const pathPrefix = "../assets/images/dragons/";
 
 let isWinAPrize = false;
 
 const MAGIC_NUMBER = 1;
-const TOTAL_RANDOM_NUMBER = 10;
+const TOTAL_RANDOM_NUMBER = 5;
 
 const bingo = {
   text: "Chúc mừng bạn!",
   images: ["water-dragon-transformed.jpg", "fire-dragon-transformed.jpg", "plant-dragon-transformed.jpg"],
 };
 
+$(window).on("load", () => {
+  $(BUTTON_PAUSE_AUDIO_SELECTOR).click(function () {
+    if (toothlessDragonAudio.paused) {
+      playAudio();
+    } else {
+      pauseAudio();
+    }
+  });
+});
+
+function playAudio() {
+  if (toothlessDragonAudio.paused === true) {
+    toothlessDragonAudio.play();
+    $(BUTTON_PAUSE_AUDIO_SELECTOR).removeClass("hidden");
+    $(BUTTON_PAUSE_AUDIO_SELECTOR).addClass("active");
+  }
+}
+
+function pauseAudio() {
+  toothlessDragonAudio.pause();
+
+  $(BUTTON_PAUSE_AUDIO_SELECTOR).removeClass("active");
+}
+
 $("[id^=envelope]").each(function () {
   $(this).on("click", function () {
-    // confetti({
-    //   particleCount: 100,
-    //   spread: 70,
-    //   origin: { x: 0, y: 0 },
-    // });
-
     if ($(this).hasClass("open")) return;
+
+    playAudio();
 
     const letterElm = $(this).children().children(".letter");
     const randNum = Math.floor(Math.random() * TOTAL_RANDOM_NUMBER);
@@ -27,13 +49,27 @@ $("[id^=envelope]").each(function () {
 
     if (randNum !== MAGIC_NUMBER || isWinAPrize) return;
 
+    pauseAudio();
+
     isWinAPrize = true;
+
+    confetti({
+      particleCount: 500,
+      spread: 50,
+      origin: { x: 0.5, y: 1 },
+    });
 
     const text = bingo.text;
     const imageSrc = pathPrefix + bingo.images[Math.floor(Math.random() * bingo.images.length)];
 
+    $(this).addClass("prize");
+
     $(letterElm).children(".text").text(text);
 
     $(letterElm).find("img").attr("src", imageSrc);
+
+    $(letterElm).on("click", function () {
+      window.location.href = "/nam-moi";
+    });
   });
 });
